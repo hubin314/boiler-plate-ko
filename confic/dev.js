3b25ef2f8cf0b5045b89aff84ea7ec66fe737dd9
hubin1739 <hubin1739@gmail.com>
2021-03-11 20:31:07 +0900

    비밀 정보 보호

---
+++ confic/dev.js
@@ -0,0 +1,3 @@
+module.exports={
+    mongoURI:'mongodb+srv://ejLee:s7oilo3BzJOvkrxN@boilerplate.yox4q.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+}
--- index.js
+++ index.js
@@ -1,20 +1,40 @@
-const express = require('express')
-const app = express()
-const port = 5000
+const express = require('express');
+const app = express();
+const port = 5000;
+const bodyParser = require('body-parser');
 
-const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://happy:Q5nwMl84OWu9Jn5y@boilerplate.yox4q.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
+const config = require('/config/key');
+
+const {User} = require("./models/User");
+
+app.use(bodyParser.urlencoded({extebded:true}));
+app.use(bodyParser.json());
+
+const mongoose = require('mongoose');
+mongoose.connect('config.mongoURI', {
   useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false
 }).then(()=>console.log('MongoDB Connected...'))
-  .catch(err => console.log(err))
+  .catch(err => console.log(err));
 
 
 
   
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Hello World!');
+})
+
+app.post('/register',(req,res) => {
+
+  const user = new User(req.body);
+
+  user.save((err, userInfo) => {
+    if(err) return res.json({ success: false, err});
+    return res.status(200).json({
+      success: true
+    })
+  })
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+  console.log(`Example app listening at http://localhost:${port}`);
 })
